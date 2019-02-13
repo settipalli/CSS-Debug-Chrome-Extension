e60b574d344054589493bee5e3276644b88f13b1
Santhoshkumar Settipalli <santhosh.settipalli@gmail.com>
2019-02-13 17:40:39 +0000

    Added background script that has access to chrome.* APIs but cannot access the current webpage loaded in chrome. Content scripts cannot access chrome.* APIs but can access the current webpage loaded in chrome. Also showcased the use of message passing to send arbitrary JSON payload from the background script to the listener in the content script.

---
+++ background.js
@@ -0,0 +1,13 @@
+// background script has access to all chrome.* APIs
+
+// content script has access to the current webpage loaded in chrome
+// background script does not have access to the current webpage loaded in chrome
+
+// Called when the user clicks on the browser action
+chrome.browserAction.onClicked.addListener(function (tab) {
+    // send a message to the active tab
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        var activeTab = tabs[0];
+        chrome.tabs.sendMessage(activeTab.id, { "message": "clicked_browser_action" });
+    });
+});
--- content.js
+++ content.js
@@ -1,3 +1,15 @@
 // content.js => a JavaScript file that runs in the context of web pages.
-var firstHref = $("a[href^='http'").eq(0).attr("href");
+
+// Content scripts have some limitations.
+// They cannot use chrome.* APIs, with the exception of extension, i18n, runtime, and storage.
+
+var firstHref = $("a[href^='http']").eq(0).attr("href");
 console.log("First Href: " + firstHref);
+
+// listen to the arbitrary JSON payload sent by background script
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        var firstHref = $("a[href^='http']").eq(0).attr("href");
+        console.log("First Href from the listener: " + firstHref);
+    }
+);
