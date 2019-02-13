12f35575d7bcb5b7116a112a8db28cb4b0fad791
Santhoshkumar Settipalli <santhosh.settipalli@gmail.com>
2019-02-13 17:49:24 +0000

    Open a new tab with the URL pointing to the value of firstHref.
    This requires communication between the background script (triggered when the user clicks on the icon) and the content script.
    
    1. Listen for a click on the browser action in background.js.
    
    2. When it’s clicked, send a clicked_browser_action event to content.js.When content.js receives the event, it grabs the URL of the first link on the page. Then it sends open_new_tab back to background.js with the URL to open.
    
    3. background.js listens for open_new_tab and opens a new tab with the given URL when it receives the message.
    
    chrome.tabs can only be used by background.js, so we’ll have to add some more message passing since background.js can open the tab, but can’t grab the URL.
    
    Clicking on the browser action will trigger background.js, which will send a message to content.js, which will send a URL back to background.js, which will open a new tab with the given URL.

--- background.js
+++ background.js
@@ -8,6 +8,17 @@ chrome.browserAction.onClicked.addListener(function (tab) {
     // send a message to the active tab
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         var activeTab = tabs[0];
-        chrome.tabs.sendMessage(activeTab.id, { "message": "clicked_browser_action" });
+        chrome.tabs.sendMessage(activeTab.id, { "message": "clicked_browser_action_event" });
     });
+
+    // Clicking on the browser action will trigger background.js, which will send a message to
+    // content.js, which will send a URL back to background.js, which will open a new tab
+    // with the given URL.
+    chrome.runtime.onMessage.addListener(
+        function (request, sender, sendResponse) {
+            if (request.message === "open_new_tab_event") {
+                chrome.tabs.create({ "url": request.url });
+            }
+        }
+    );
 });
--- content.js
+++ content.js
@@ -9,7 +9,13 @@ console.log("First Href: " + firstHref);
 // listen to the arbitrary JSON payload sent by background script
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
-        var firstHref = $("a[href^='http']").eq(0).attr("href");
-        console.log("First Href from the listener: " + firstHref);
+        if (request.message === "clicked_browser_action_event") {
+            var firstHref = $("a[href^='http']").eq(0).attr("href");
+            console.log("First Href from the listener: " + firstHref);
+
+            // send the URL to background.js so that it could open it in a new tab
+            // content script cannot open new tabs
+            chrome.runtime.sendMessage({ "message": "open_new_tab_event", "url": firstHref });
+        }
     }
 );
